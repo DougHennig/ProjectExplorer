eba41861fbf73ec72929cfaa8f875e48a7fba123
Doug Hennig <dhennig@stonefield.com>
2018-01-09 16:34:32 -0600

    Worked on support for other binary-to-text converters besides FoxBin2PRG and to handle FoxBin2PRG config settings

--- Source/projectexplorer.h
+++ Source/projectexplorer.h
@@ -4,7 +4,6 @@
 	&& the HKEY_CURRENT_USER location for VFP options
 #define ccPROJECT_EXPLORER_KEY			'Software\ProjectExplorer'
 	&& the HKEY_CURRENT_USER location for Project Explorer options
-#define ccFOXBIN2PRG_MAIN_EXE_FILENAME	'FoxBin2Prg.EXE'
 
 #define ccSOLUTION_FILE					'Solution.xml'
 	&& the name of the solution file
