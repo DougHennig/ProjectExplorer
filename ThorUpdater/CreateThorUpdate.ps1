1a4b7953d4c99db4ce3e09d39e410310b1760872
Doug Hennig <doug@doughennig.com>
2022-05-15 08:25:59 -0500

    Fixed ThorUpdater issue

--- ThorUpdater/CreateThorUpdate.ps1
+++ ThorUpdater/CreateThorUpdate.ps1
@@ -29,19 +29,45 @@ try
     # and add them to a zip file.
     # See https://stackoverflow.com/questions/15294836/how-can-i-exclude-multiple-folders-using-get-childitem-exclude
     # for how to exclude folders when -Recurse is used
-    Get-ChildItem . -Recurse -File -Exclude $excludeFiles | %{ 
-        $allowed = $true
-        foreach ($exclude in $excludeFolders) { 
-            if ((Split-Path $_.FullName -Parent) -ilike $exclude) { 
-                $allowed = $false
-                break
+    $files = @(Get-ChildItem . -recurse -file -exclude $excludeFiles |
+        %{ 
+            $allowed = $true
+            foreach ($exclude in $excludeFolders)
+            { 
+                if ((Split-Path $_.FullName -Parent) -ilike $exclude)
+                { 
+                    $allowed = $false
+                    break
+                }
+            }
+            if ($allowed)
+            {
+                $_
             }
         }
-        if ($allowed) {
-            $_
-        }
-    } | Compress-Archive -CompressionLevel "Fastest" -DestinationPath $zipFileName
+    );
 
+    # See https://stackoverflow.com/questions/51392050/compress-archive-and-preserve-relative-paths to compress
+    # exclude directory entries and generate fullpath list
+    $filesFullPath = $files | Where-Object -Property Attributes -CContains Archive | ForEach-Object -Process {Write-Output -InputObject $_.FullName}
+
+    #create zip file
+    Add-Type -AssemblyName System.IO.Compression, System.IO.Compression.FileSystem
+    $zip = [System.IO.Compression.ZipFile]::Open((Join-Path -Path $(Resolve-Path -Path ".") -ChildPath $zipFileName), [System.IO.Compression.ZipArchiveMode]::Create)
+
+    #write entries with relative paths as names
+    foreach ($fname in $filesFullPath)
+    {
+        $rname = $(Resolve-Path -Path $fname -Relative) -replace '\.\\',''
+        $zentry = $zip.CreateEntry($rname)
+        $zentryWriter = New-Object -TypeName System.IO.BinaryWriter $zentry.Open()
+        $zentryWriter.Write([System.IO.File]::ReadAllBytes($fname))
+        $zentryWriter.Flush()
+        $zentryWriter.Close()
+    }
+
+    # clean up
+    Get-Variable -exclude Runspace | Where-Object {$_.Value -is [System.IDisposable]} | Foreach-Object {$_.Value.Dispose(); Remove-Variable $_.Name};
 
     # If the zip file was created, read in the Version source file, replace
     # placeholders, and write out the project version file.
